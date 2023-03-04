b3426ef48ab0a7b3681e36bf468a7ff0d2f5475c
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-04 20:20:43 -0300

    Update index.js

--- Javascripit/ex 005/index.js
+++ Javascripit/ex 005/index.js
@@ -3,7 +3,10 @@ const result= document.querySelector('#result')
 function caraCoroa(num){
     return Math.round(Math.random(num) * num);
 }
-//console.log(moeda[caraCoroa(1)]);
-const h3 = document.createElement('h3')
-h3.appendChild('h3');
-result.innerHTML = moeda[caraCoroa(1)];
+function resultado(){
+    const h3 = document.createElement('h3')
+    result.appendChild(h3);
+    return result.innerHTML = moeda[caraCoroa(1)];
+}
+
+

757a9ab66f1d9660d8744309f70db333e869df8c
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-05 18:02:56 -0300

    Update index.js

--- Javascript/ex 005/ex moeda/index.js
+++ Javascript/ex 005/ex moeda/index.js
@@ -6,7 +6,14 @@ function caraCoroa(num){
 function resultado(){
     const h3 = document.createElement('h3')
     result.appendChild(h3);
-    return result.innerHTML = moeda[caraCoroa(1)];
+    result.innerHTML = moeda[caraCoroa(1)];
 }
+
+const intervalo = setInterval(function(){
+    
+    const int = document.createElement('h3')
+    result.appendChild(int);
+    result.innerHTML = "..."
+},500)
 
 

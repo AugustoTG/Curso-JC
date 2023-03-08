adb50231972e428a33e976df628fd8af333bdc4d
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-07 21:43:21 -0300

    Update index.js

--- Javascript/ex 004/index.js
+++ Javascript/ex 004/index.js
@@ -1,14 +1,20 @@
-    let resut = document.querySelector('#result');
-    let clue = document.querySelector('#clue');
-function getNumber(){
+function guessNumber(max){
+    return Math.floor(Math.random() * max);
+}
+console.log(guessNumber(101));
+
+let resut = document.querySelector('#result');
+let clue = document.querySelector('#clue');
+
+function getNumber(){  
     let num = document.querySelector('#inputNumber');
     num = Number(num.value)
-    return num;
-
+    console.log(num)
 }
-
-function guessNumber(max){
-    return Math.round(Math.random() * max);
+console.log(getNumber());
+if(guessNumber(101 === getNumber)){
+    console.log('vc acertou')
+}else{
+    console.log('vc errou')
 }
-
 

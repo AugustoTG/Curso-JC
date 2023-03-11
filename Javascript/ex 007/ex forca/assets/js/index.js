4a396644dedbd57246f061a5912ad6fa8f71f428
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-11 11:33:46 -0300

    Update index.js

--- Javascript/ex 007/ex forca/assets/js/index.js
+++ Javascript/ex 007/ex forca/assets/js/index.js
@@ -1,10 +1,21 @@
 const palavras = ['sagaz', 'negro', 'plena', 'poder', 'anexo', 'brado'];
+const result = document.querySelector('h2');
+const inf = document.querySelector('p')
 function guessNumber(max){
     return Math.floor(Math.random() * max);
 }
-console.log(palavras[guessNumber(6)]);
-const result = document.querySelector('h2')
+const palavra = palavras[guessNumber(6)];
+console.log(palavra);
+
 const verificador = () => {
     const input = document.querySelector('#texto');
-   
+    if(input.value === palavra){
+        inf.innerText = 'Parabéns você acertou!'
+        return result.innerText = palavra, inf.innerText ='Parabéns você acertou!';
+    }else if(input.value !== palavra){
+        if(input.value.length !== 1){
+            alert('Insira apenas uma letra ou se você souber a resposta, escreva!')
+        };
+    };
+    
 };

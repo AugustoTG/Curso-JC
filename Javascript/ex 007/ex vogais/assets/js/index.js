d943e7b1d6814452a37db89ac59535fbbe77db00
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-10 21:28:38 -0300

    Update index.js

--- Javascript/ex 007/ex vogais/assets/js/index.js
+++ Javascript/ex 007/ex vogais/assets/js/index.js
@@ -1,5 +1,14 @@
-setInterval(function(){
-    const data = new Date();
-    const resultado = document.querySelector('.container h1');
-    resultado.innerText = data.toLocaleString('pt-BR', {dateStyle: 'full', timeStyle: 'medium'});
-}, 1000)
+const result = document.querySelector('p')
+console.log(result)
+const verificador = () => {
+    const input = document.querySelector('#texto');
+    if(input.value.length !== 1){
+        console.log(input.length)
+        return alert('Insira apenas uma vogal ou consoante')
+    }
+    if(input.value === 'a' || input.value === 'e' || input.value === 'i' || input.value === 'o' || input.value === 'u' || input.value === 'A' || input.value === 'E' || input.value === 'I' || input.value === 'O' || input.value === 'U'){
+        return result.innerHTML = `${input.value} é uma vogal!`
+    }else{
+        return result.innerHTML = `${input.value} Não é uma vogal!`
+    }
+};

775f387666b1317c45aadd9151750f130b74c61f
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-10 22:48:55 -0300

    Update index.js

--- Javascript/ex 007/ex forca/assets/js/index.js
+++ Javascript/ex 007/ex forca/assets/js/index.js
@@ -1,15 +1,10 @@
 const palavras = ['sagaz', 'negro', 'plena', 'poder', 'anexo', 'brado'];
-const result = document.querySelector('p')
-console.log(result)
+function guessNumber(max){
+    return Math.floor(Math.random() * max);
+}
+console.log(palavras[guessNumber(6)]);
+const result = document.querySelector('h2')
 const verificador = () => {
     const input = document.querySelector('#texto');
-    if(input.value.length !== 1){
-        console.log(input.length)
-        return alert('Insira apenas uma vogal ou consoante')
-    }
-    if(input.value === 'a' || input.value === 'e' || input.value === 'i' || input.value === 'o' || input.value === 'u' || input.value === 'A' || input.value === 'E' || input.value === 'I' || input.value === 'O' || input.value === 'U'){
-        return result.innerHTML = `${input.value} é uma vogal!`
-    }else{
-        return result.innerHTML = `${input.value} Não é uma vogal!`
-    }
+   
 };

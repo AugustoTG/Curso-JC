e8921f15e8fefe3efd8269e778f93029542ec5e3
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-14 18:03:03 -0300

    Update index.js

--- Javascript/ex 007/ex vogais/assets/js/index.js
+++ Javascript/ex 007/ex vogais/assets/js/index.js
@@ -2,11 +2,12 @@ const result = document.querySelector('p')
 console.log(result)
 const verificador = () => {
     const input = document.querySelector('#texto');
-    if(input.value.length !== 1){
+    if(input?.value?.length !== 1){
         console.log(input.length)
         return alert('Insira apenas uma vogal ou consoante')
     }
-    const vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    const vogaisMin = ['a', 'e', 'i', 'o', 'u'];
+    const vogais = vogaisMin.toLowerCase();
     for(let vogal of vogais){
         if(input.value === vogal){
             return result.innerHTML = `${input.value} é uma vogal!`;
